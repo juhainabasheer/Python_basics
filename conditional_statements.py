ba132be7b7dfeb2a2d38cbcdb48d85da2b31b7d5
juhainabasheer <juhainabk@gmail.com>
2025-08-08 20:01:06 +0530

    conditional statement

---
+++ conditional_statements.py
@@ -0,0 +1,24 @@
+"""
+
+Syntax 
+
+if condition:
+    statements
+
+"""
+
+"""age=int(input("enter the age:"))
+if age>=18:
+    print("eligible for voting")
+else:
+    print("not eligible for voting")"""
+
+mark=int(input("enter the mark"))
+if mark>=95:
+    print("excellent")
+elif mark>=75:
+    print("good job")
+elif mark>=50:
+    print("You secured c grade")
+else:
+    print("keep practicing")
